0fd069efbc0f7c7285fbbdafad56e79241dc310f
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 18:43:05 +0000

    refactor: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript'],
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
-        'import/prefer-default-export': 'off',
-        'no-param-reassign': 'off',
-    },
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-    },
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'implicit-arrow-linebreak': 'off',
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
 };

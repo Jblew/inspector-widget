df92c18f35ac5569d7702c6cf64f645d4b6d6aa6
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 17:34:44 +0000

    feat: setup aws-cost-ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'import/prefer-default-export': 'off',
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
+        'import/prefer-default-export': 'off',
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+    },
 };

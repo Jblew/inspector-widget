79a98c2faf15c4f01222cf43588b2860ca1d2ce3
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 17:36:25 +0000

    fix: fix roles deployment

--- public/roles/src/index.js
+++ public/roles/src/index.js
@@ -1,14 +1,14 @@
-const { RolesConfig, FIREBASE_CONFIG } = require("../../../config");
+const { RolesConfig, FIREBASE_CONFIG } = require('../../../config/dist/config');
 
-const { FirestoreRolesManagerAppUI } = require("firestore-roles-manager-ui");
+const { FirestoreRolesManagerAppUI } = require('firestore-roles-manager-ui');
 
 const config = {
-  title: "Inspector Widget role management",
-  firebase: FIREBASE_CONFIG,
-  authProviders: ["google.com"],
-  roles: RolesConfig,
-  basePath: "/roles/",
-  menuLinks: []
+    title: 'Inspector Widget role management',
+    firebase: FIREBASE_CONFIG,
+    authProviders: ['google.com'],
+    roles: RolesConfig,
+    basePath: '/roles/',
+    menuLinks: [],
 };
 
-FirestoreRolesManagerAppUI.mount("#app", config);
+FirestoreRolesManagerAppUI.mount('#app', config);

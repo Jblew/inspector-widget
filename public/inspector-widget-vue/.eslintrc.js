1d661b033ae78389cf928f3db99a09f73cf8dc75
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 15:42:56 +0000

    chore: configure linting

--- public/inspector-widget-vue/.eslintrc.js
+++ public/inspector-widget-vue/.eslintrc.js
@@ -3,14 +3,11 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/prefer-default-export': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',

d04fbde247434ab5f95ef3022240d6d9ec24d0b6
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 17:12:48 +0000

    ci: fix rule generator

--- config/roles.ts
+++ config/roles.ts
@@ -1,15 +1,18 @@
-import { RoleKey } from "./RoleKey";
-import { FIRESTORE_COLLECTIONS } from "./firestore-collections";
+import { RoleKey } from './RoleKey';
+import { FIRESTORE_COLLECTIONS } from './firestore-collections';
 
 export const RolesConfig = Object.freeze({
-  accountsCollection: FIRESTORE_COLLECTIONS.accounts,
-  roleCollectionPrefix: "role_",
-  roleRequestsCollectionPrefix: "rolereq_",
-  roles: {
-    [RoleKey.admin]: {
-      manages: [RoleKey.viewer]
-    }
-  }
+    accountsCollection: FIRESTORE_COLLECTIONS.accounts,
+    roleCollectionPrefix: 'role_',
+    roleRequestsCollectionPrefix: 'rolereq_',
+    roles: {
+        [RoleKey.admin]: {
+            manages: [RoleKey.viewer],
+        },
+        [RoleKey.viewer]: {
+            manages: [],
+        },
+    },
 });
 
 export default RolesConfig;
